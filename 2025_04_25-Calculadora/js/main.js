f8cef3b5f6123f6db876805d462548e608f6bd68
Nuria Campo <nuria.campo7@gmail.com>
2025-04-24 10:02:54 +0200

    tutoría calculadora

---
+++ 2025_04_25-Calculadora/js/main.js
@@ -0,0 +1,47 @@
+"use strict";
+/*
+Recoger los elementos del html:
+- los dos input
+- select
+- botón
+- h3 resultado
+
+Cuando la usuaria haga click en calcular
+  - recojo los valores de los inputs
+  - recoger el valor del select
+    - si el operador es +
+        - sumo
+    - si el operador el -
+        - resto
+    - si el operador el *
+        - multiplico
+    - si el operador el /
+        - divido  
+  - pinto el resultado de la operación
+*/
+
+const num1Input = document.querySelector(".js-number1");
+const num2Input = document.querySelector(".js-number2");
+const operator = document.querySelector(".js-operator");
+const button = document.querySelector(".js-button");
+const resultCalculator = document.querySelector(".js-result");
+
+const handleClick = (event) => {
+  event.preventDefault();
+  const num1 = parseInt(num1Input.value);
+  const num2 = parseInt(num2Input.value);
+
+  let result;
+  if (operator.value === "sumar") {
+    result = num1 + num2;
+  } else if (operator.value === "restar") {
+    result = num1 - num2;
+  } else if (operator.value === "multiplicar") {
+    result = num1 * num2;
+  } else if (operator.value === "dividir") {
+    result = num1 / num2;
+  }
+
+  resultCalculator.innerHTML = `El resultado es ${result}`;
+};
+button.addEventListener("click", handleClick);
